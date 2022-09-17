7b7ef62b8a1364a3abef2b950f564cd65f74ceba
Zoella7 <yuliahavrylyshyn1@gmail.com>
2022-09-17 23:10:08 +0300

    updates

--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -4,7 +4,7 @@ import {urls} from "../constants";
 const movieService = {
     getAll: (page = 1) => axiosService.get(urls.movies, {params: {page}}),
     getById: (id) => axiosService.get(`${urls.genreByID+id}`),
-    search:(obj) => axiosService.get(`${urls.search+obj}`)
+    search: (obj) => axiosService.get(`${urls.search+obj}`)
 
 }
 export {movieService}
